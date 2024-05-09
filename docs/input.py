70e77bcf5ead3d8173be72725188bbda0a2545c8
Pascal Gitz <pascal.gitz@hotmail.ch>
2024-05-09 13:09:19 +0200

    vorspanngeometrie

--- docs/input.py
+++ docs/input.py
@@ -57,6 +57,8 @@ los = ureg.dimensionless
 import scipy.integrate as integrate
 from scipy.optimize import fsolve
 from scipy.misc import derivative
+from scipy.optimize import curve_fit
+
 import handcalcs.render
 
 ## Plotting und display
@@ -69,32 +71,5 @@ set_engineering_style()
 from IPython.display import Latex, Markdown
 
 import matplotlib.pyplot as plt
-
-
-
-
-# # Parameter
-
-# params_kragarm = {
-#     'l_Kragarm':5*unit.meter,
-#     'F1':-10000*unit.N,
-#     'F2':-21500*unit.N,
-#     'E':10000*unit.N/unit.mm**2,
-#     'h':400*unit.mm,
-#     'b':200*unit.mm,
-#     'k_1': 100000*unit.N/unit.meter,
-#     'k_2':10000*unit.N/unit.meter,
-#     'z': 400*unit.mm,
-# }
-
-# params_value_kragarm = to_float(params_kragarm)
-
-# params_a3v2 = {
-#     'l':2.62*unit.meter, 
-#     'F_max':-320*unit.kN, 
-#     'l_spring':2.*unit.mm,
-# }
-
-# params_value_a3v2 = to_float(params_a3v2)
 
 
